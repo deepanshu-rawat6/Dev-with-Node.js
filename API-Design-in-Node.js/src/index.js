1fd2b2b0e30b966893f5ccca4c390d874e28e967
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-09-29 22:46:21 +0530

    update: added README.md

--- API-Design-in-Node.js/src/index.js
+++ API-Design-in-Node.js/src/index.js
@@ -1,12 +1,14 @@
 import http from 'http';
 
-const server = http.createServer( async (req, res) => {
+const server = http.createServer(async (req, res) => {
+    console.log(req)
+    console.log(res)
     if (req.method === 'GET' && req.url === '/') {
-        res.statusCode(200)
+        console.log("Hello form the server")
         res.end()
     }
 })
 
 server.listen(3001, () => {
-    console.log
+    console.log('server on http://localhost:3001')
 })
